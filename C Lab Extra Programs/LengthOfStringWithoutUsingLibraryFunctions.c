d074a8864b64055c62785e569b94e9c3712b88b4
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-02-13 13:34:42 +0530

    13/02/2024

---
+++ C Lab Extra Programs/LengthOfStringWithoutUsingLibraryFunctions.c
@@ -0,0 +1,17 @@
+// Program to get the length of string without using string library functions
+#include<stdio.h>
+int main() {
+    char str[100];
+    int length=0;
+    printf("Enter anything : ");
+    gets(str);
+    for(int i=0; str[i] != '\0'; i++) {
+        if(str[i] == '\0') {
+            break;
+        }else{
+            length++;
+        }
+    }
+    printf("Length of string is %d", length);
+    return 0;
+}
