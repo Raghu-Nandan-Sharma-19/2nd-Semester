f24bab0fa4d05c9486e157d875b5323256d73b43
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-02-15 10:59:36 +0530

    15/02/2024

---
+++ C Lab Extra Programs/MaxOfThreeNumbers.c
@@ -0,0 +1,20 @@
+// Max of three numbers using nested if 
+#include<stdio.h>
+void maximumOfThreeNumbers(int num1, int num2, int num3) {
+    if(num1>num2 || num2>num3 || num3>num2) {
+        if(num1>num2 && num1>num3) {
+            printf("%d is greatest.", num1);
+        } else if(num2>num1 && num2>num3) {
+            printf("%d is greatest.", num2);
+        } else {
+            printf("%d is greatest.", num3);
+        }
+    }
+}
+int main() {
+    int num1, num2, num3;
+    printf("Enter three numbers : \n");
+    scanf("%d%d%d", &num1, &num2, &num3);
+    maximumOfThreeNumbers(num1, num2, num3);
+    return 0;
+}
